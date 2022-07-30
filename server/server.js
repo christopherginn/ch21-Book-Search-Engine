2e87be9e49ae23000fcdf0f73d1198740904e681
christopherginn <hey.im.chris@live.com>
2022-07-30 11:04:27 -0400

    set up files for schemas directory

--- server/server.js
+++ server/server.js
@@ -33,6 +33,7 @@ const startApolloServer = async (typeDefs, resolvers) => {
 
   db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 };
 
