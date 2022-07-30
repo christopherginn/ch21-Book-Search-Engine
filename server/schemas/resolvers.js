3152355a4a0c42fc6d895ae7d3f1ec15e1516ffa
christopherginn <hey.im.chris@live.com>
2022-07-30 12:27:25 -0400

    wrote most of resolvers.js. Need to make resolver for saveBook

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,59 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+       me: async (parent, args, context) => {
+        if (context.user) {
+            const meData = 
+                await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                // .populate('books')
+            return meData;
+        }
+        throw new AuthenticationError('You are not logged in!');
+       }
+    },
+
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError('No user with this email found!')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password!')
+            }
+
+            const token = signToken(user);
+            return { token, user }
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user }
+        },
+
+        saveBook: async (parent, { input }, context) => {
+
+        },
+
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedBooks = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                )
+                return updatedBooks; 
+            }
+            throw new AuthenticationError('You are not logged in!')
+        },
+
+
+    }
+};
